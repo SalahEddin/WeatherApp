aafd64648d9bdacec3d44aa26c5ff644c6615c36
SalahEddin <salshaal@gmail.com>
2015-11-07 12:24:57 +0300

    Added connection to sqlite database from Servlet

--- src/java/WebWeatherDebug.java
+++ src/java/WebWeatherDebug.java
@@ -6,6 +6,13 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -28,19 +35,61 @@ public class WebWeatherDebug extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+        
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
+            
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet WebWeatherDebug</title>");            
+            out.println("<title>Servlet WebWeatherDebug</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet WebWeatherDebug at " + request.getContextPath() + "</h1>");
+            
+            // Connection
+            Connection conn = null;
+            try{
+                Class.forName("org.sqlite.JDBC");
+                conn = DriverManager.getConnection("jdbc:sqlite:../../WeatherDB.sqlite");
+                Statement stmt = conn.createStatement();
+                String query = "SELECT * FROM LOCATION";
+                ResultSet rs = stmt.executeQuery(query);
+                while (rs.next()) {
+                    out.println("<p>");
+                    out.println(rs.getString("NAME"));
+                    out.println("</p>");
+                }
+            }
+            catch(SQLException e){System.out.print(e.toString());}
+            catch(Exception e){System.out.print(e.toString());}
+            finally{
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(WebWeatherDebug.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
+            out.println(
+            "<table style=\"width:100%\">\n" +
+            "  <tr>\n" +
+            "    <td>Jill</td>\n" +
+            "    <td>Smith</td>		\n" +
+            "    <td>50</td>\n" +
+            "  </tr>\n" +
+            "  <tr>\n" +
+            "    <td>Eve</td>\n" +
+            "    <td>Jackson</td>		\n" +
+            "    <td>94</td>\n" +
+            "  </tr>\n" +
+            "</table>"
+            );
             out.println("</body>");
             out.println("</html>");
         }
+        
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -69,7 +118,8 @@ public class WebWeatherDebug extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        response.setContentType("text/html;charset=UTF-8");
+        //processRequest(request, response);
     }
 
     /**
